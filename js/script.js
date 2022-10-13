a8b9d821914e0b75712c2fb3d5e5e7a2b6b5cbb5
Luis Morillo <luis_cnnvd@hotmail.com>
2022-10-13 20:15:20 +0200

    multiple changes to adapt css and newer version of ghc

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@
     checkbox.checked = false;
   }, false);
 })(document);
+
+
+(function (document) {
+  //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+  var dropdown = document.getElementById("chapter-list");
+
+  dropdown.addEventListener("click", function () {
+
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+})(document);
--- site.hs
+++ site.hs
@@ -3,195 +3,196 @@
 import           Data.Monoid                   (mappend)
 import           Data.List                     (sortBy)
 import           Data.Ord                      (comparing)
-import           Hakyll
+import qualified Hakyll
 import           Control.Monad                 (liftM, forM_)
 import           System.FilePath               (takeBaseName)
-
+import           Data.Function (on)
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
-    match ("images/*" .||. "js/*") $ do
-        route   idRoute
-        compile copyFileCompiler
+main = Hakyll.hakyll $ do
+    Hakyll.match ("images/*" Hakyll..||. "js/*") $ do
+        Hakyll.route   Hakyll.idRoute
+        Hakyll.compile Hakyll.copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+    Hakyll.match "css/*" $ do
+        Hakyll.route   Hakyll.idRoute
+        Hakyll.compile Hakyll.compressCssCompiler
 
-    match "error/*" $ do
-        route $ (gsubRoute "error/" (const "") `composeRoutes` setExtension "html")
-        compile $ pandocCompiler
-            >>= applyAsTemplate siteCtx
-            >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> siteCtx)
+    Hakyll.match "error/*" $ do
+        Hakyll.route (Hakyll.gsubRoute "error/" (const "") `Hakyll.composeRoutes` Hakyll.setExtension "html")
+        Hakyll.compile $ Hakyll.pandocCompiler
+            >>= Hakyll.applyAsTemplate siteCtx
+            >>= Hakyll.loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> siteCtx)
 
-    match "pages/*" $ do
-        route $ setExtension "html"
-        compile $ do
-            pageName <- takeBaseName . toFilePath <$> getUnderlying
-            let pageCtx = constField pageName "" `mappend`
+    Hakyll.match "pages/*" $ do
+        Hakyll.route $ Hakyll.setExtension "html"
+        Hakyll.compile $ do
+            pageName <- takeBaseName . Hakyll.toFilePath <$> Hakyll.getUnderlying
+            let pageCtx = Hakyll.constField pageName "" `mappend`
                           baseNodeCtx
-            let evalCtx = functionField "get-meta" getMetadataKey `mappend`
-                          functionField "eval" (evalCtxKey pageCtx)
+            let evalCtx = Hakyll.functionField "get-meta" getMetadataKey `mappend`
+                          Hakyll.functionField "eval" (evalCtxKey pageCtx)
             let activeSidebarCtx = sidebarCtx (evalCtx <> pageCtx)
 
-            pandocCompiler
-                >>= saveSnapshot "page-content"
-                >>= loadAndApplyTemplate "templates/page.html"    siteCtx
-                >>= loadAndApplyTemplate "templates/default.html" (activeSidebarCtx <> siteCtx)
-                >>= relativizeUrls
+            Hakyll.pandocCompiler
+                >>= Hakyll.saveSnapshot "page-content"
+                >>= Hakyll.loadAndApplyTemplate "templates/page.html"    siteCtx
+                >>= Hakyll.loadAndApplyTemplate "templates/default.html" (activeSidebarCtx <> siteCtx)
+                >>= Hakyll.relativizeUrls
 
-    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+    tags <- Hakyll.buildTags "posts/*" (Hakyll.fromCapture "tags/*.html")
 
-    match "posts/*" $ version "meta" $ do
-        route   $ setExtension "html"
-        compile getResourceBody
+    Hakyll.match "posts/*" $ Hakyll.version "meta" $ do
+        Hakyll.route   $ Hakyll.setExtension "html"
+        Hakyll.compile Hakyll.getResourceBody
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ do
-            posts <- loadAll ("posts/*" .&&. hasVersion "meta")
-            let taggedPostCtx = (tagsField "tags" tags) `mappend`
+    Hakyll.match "posts/*" $ do
+        Hakyll.route $ Hakyll.setExtension "html"
+        Hakyll.compile $ do
+            posts <- Hakyll.loadAll ("posts/*" Hakyll..&&. Hakyll.hasVersion "meta")
+            let taggedPostCtx = Hakyll.tagsField "tags" tags `mappend`
                                 postCtx `mappend`
-                                (relatedPostsCtx posts 3)
+                                relatedPostsCtx posts 3
 
-            pandocCompiler
-                >>= saveSnapshot "content"
-                >>= loadAndApplyTemplate "templates/post.html" taggedPostCtx
-                >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> siteCtx)
-                >>= relativizeUrls
+            Hakyll.pandocCompiler
+                >>= Hakyll.saveSnapshot "content"
+                >>= Hakyll.loadAndApplyTemplate "templates/post.html" taggedPostCtx
+                >>= Hakyll.loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> siteCtx)
+                >>= Hakyll.relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAllSnapshots ("posts/*" .&&. hasNoVersion) "content"
+    Hakyll.create ["archive.html"] $ do
+        Hakyll.route Hakyll.idRoute
+        Hakyll.compile $ do
+            posts <- Hakyll.recentFirst =<< Hakyll.loadAllSnapshots ("posts/*" Hakyll..&&. Hakyll.hasNoVersion) "content"
             let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archive"             `mappend`
-                    constField "archive" ""                  `mappend`
+                    Hakyll.listField "posts" postCtx (return posts) `mappend`
+                    Hakyll.constField "title" "Archive"             `mappend`
+                    Hakyll.constField "archive" ""                  `mappend`
                     siteCtx
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> archiveCtx)
-                >>= relativizeUrls
+            Hakyll.makeItem ""
+                >>= Hakyll.loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= Hakyll.loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> archiveCtx)
+                >>= Hakyll.relativizeUrls
 
-    paginate <- buildPaginateWith postsGrouper "posts/*" postsPageId
+    paginate <- Hakyll.buildPaginateWith postsGrouper "posts/*" postsPageId
 
-    paginateRules paginate $ \page pattern -> do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAllSnapshots (pattern .&&. hasNoVersion) "content"
+    Hakyll.paginateRules paginate $ \page pattern -> do
+        Hakyll.route Hakyll.idRoute
+        Hakyll.compile $ do
+            posts <- Hakyll.recentFirst =<< Hakyll.loadAllSnapshots (pattern Hakyll..&&. Hakyll.hasNoVersion) "content"
             let indexCtx =
-                    constField "title" (if page == 1 then "Home"
+                    Hakyll.constField "title" (if page == 1 then "Home"
                                                      else "Blog posts, page " ++ show page) `mappend`
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "home" "" `mappend`
-                    paginateContext paginate page `mappend`
+                    Hakyll.listField "posts" postCtx (return posts) `mappend`
+                    Hakyll.constField "home" "" `mappend`
+                    Hakyll.paginateContext paginate page `mappend`
                     siteCtx
 
-            makeItem ""
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/index.html" indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> indexCtx)
-                >>= relativizeUrls
+            Hakyll.makeItem ""
+                >>= Hakyll.applyAsTemplate indexCtx
+                >>= Hakyll.loadAndApplyTemplate "templates/index.html" indexCtx
+                >>= Hakyll.loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> indexCtx)
+                >>= Hakyll.relativizeUrls
 
-    match "templates/*" $ compile templateBodyCompiler
+    Hakyll.match "templates/*" $ Hakyll.compile Hakyll.templateBodyCompiler
 
-    create ["atom.xml"] $ do
-        route idRoute
-        compile $ do
+    Hakyll.create ["atom.xml"] $ do
+        Hakyll.route Hakyll.idRoute
+        Hakyll.compile $ do
             let feedCtx = postCtx `mappend`
-                    bodyField "description"
-            posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots ("posts/*" .&&. hasNoVersion) "content"
-            renderAtom feedConfig feedCtx posts
+                    Hakyll.bodyField "description"
+            posts <- fmap (take 10) . Hakyll.recentFirst =<< Hakyll.loadAllSnapshots ("posts/*" Hakyll..&&. Hakyll.hasNoVersion) "content"
+            Hakyll.renderAtom feedConfig feedCtx posts
 
 --------------------------------------------------------------------------------
 
-postsGrouper :: MonadMetadata m => [Identifier] -> m [[Identifier]]
-postsGrouper = liftM (paginateEvery 3) . sortRecentFirst
+postsGrouper :: (Hakyll.MonadMetadata m, MonadFail m) => [Hakyll.Identifier] -> m [[Hakyll.Identifier]]
+postsGrouper = fmap (Hakyll.paginateEvery 3) . Hakyll.sortRecentFirst
 
-postsPageId :: PageNumber -> Identifier
-postsPageId n = fromFilePath $ if (n == 1) then "index.html" else show n ++ "/index.html"
+postsPageId :: Hakyll.PageNumber -> Hakyll.Identifier
+postsPageId n = Hakyll.fromFilePath $ if n == 1 then "index.html" else show n ++ "/index.html"
 
 --------------------------------------------------------------------------------
 
-feedConfig :: FeedConfiguration
-feedConfig = FeedConfiguration
-    { feedTitle       = "lanyon-hakyll: Lanyon Theme on Hakyll"
-    , feedDescription = "A Fork of Lanyon based on Poole"
-    , feedAuthorName  = "Heuna Kim"
-    , feedAuthorEmail = "ai@heuna-kim.net"
-    , feedRoot        = "https://github.com/hahey/lanyon-hakyll"
+feedConfig :: Hakyll.FeedConfiguration
+feedConfig = Hakyll.FeedConfiguration
+    { Hakyll.feedTitle       = "lanyon-hakyll: Lanyon Theme on Hakyll"
+    , Hakyll.feedDescription = "A Fork of Lanyon based on Poole"
+    , Hakyll.feedAuthorName  = "Heuna Kim"
+    , Hakyll.feedAuthorEmail = "ai@heuna-kim.net"
+    , Hakyll.feedRoot        = "https://github.com/hahey/lanyon-hakyll"
     }
 
 --------------------------------------------------------------------------------
 
-siteCtx :: Context String
+siteCtx :: Hakyll.Context String
 siteCtx =
     baseCtx `mappend`
-    constField "site_description" "Lanyon Theme on Hakyll" `mappend`
-    constField "site-url" "https://github.com/hahey/lanyon-hakyll" `mappend`
-    constField "tagline" "A Fork of Lanyon based on Poole" `mappend`
-    constField "site-title" "lanyon-hakyll" `mappend`
-    constField "copy-year" "2020" `mappend`
-    constField "github-repo" "https://github.com/hahey/lanyon-hakyll" `mappend`
-    defaultContext
+    Hakyll.constField "site_description" "Lanyon Theme on Hakyll" `mappend`
+    Hakyll.constField "site-url" "https://github.com/hahey/lanyon-hakyll" `mappend`
+    Hakyll.constField "tagline" "A Fork of Lanyon based on Poole" `mappend`
+    Hakyll.constField "site-title" "lanyon-hakyll" `mappend`
+    Hakyll.constField "copy-year" "2020" `mappend`
+    Hakyll.constField "github-repo" "https://github.com/hahey/lanyon-hakyll" `mappend`
+    Hakyll.defaultContext
 
+baseCtx :: Hakyll.Context a
 baseCtx =
-    constField "baseurl" "http://localhost:8000"
+    Hakyll.constField "baseurl" "http://localhost:8000"
 
 --------------------------------------------------------------------------------
 
-postCtx :: Context String
+postCtx :: Hakyll.Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+    Hakyll.dateField "date" "%B %e, %Y" `mappend`
+    Hakyll.defaultContext
 
+tagsRulesVersioned :: Hakyll.Tags -> (String -> [Hakyll.Identifier] -> Hakyll.Rules ()) -> Hakyll.Rules ()
 tagsRulesVersioned tags rules =
-    forM_ (tagsMap tags) $ \(tag, identifiers) ->
-        rulesExtraDependencies [tagsDependency tags] $
-            create [tagsMakeId tags tag] $
+    forM_ (Hakyll.tagsMap tags) $ \(tag, identifiers) ->
+        Hakyll.rulesExtraDependencies [Hakyll.tagsDependency tags] $
+            Hakyll.create [Hakyll.tagsMakeId tags tag] $
                 rules tag identifiers
 
 relatedPostsCtx
-  :: [Item String]  -> Int  -> Context String
-relatedPostsCtx posts n = listFieldWith "related_posts" postCtx selectPosts
+  :: [Hakyll.Item String]  -> Int  -> Hakyll.Context String
+relatedPostsCtx posts n = Hakyll.listFieldWith "related_posts" postCtx selectPosts
   where
-    rateItem ts i = length . filter (`elem` ts) <$> (getTags $ itemIdentifier i)
+    rateItem ts i = length . filter (`elem` ts) <$> Hakyll.getTags (Hakyll.itemIdentifier i)
     selectPosts s = do
-      postTags <- getTags $ itemIdentifier s
+      postTags <- Hakyll.getTags $ Hakyll.itemIdentifier s
       let trimmedItems = filter (not . matchPath s) posts
       take n . reverse <$> sortOnM (rateItem postTags) trimmedItems
 
-matchPath :: Item String -> Item String -> Bool
-matchPath x y = eqOn (toFilePath . itemIdentifier) x y
-
-eqOn :: Eq b => (a -> b) -> a -> a -> Bool
-eqOn f x y = f x == f y
+matchPath :: Hakyll.Item String -> Hakyll.Item String -> Bool
+matchPath = (==) `on` (Hakyll.toFilePath . Hakyll.itemIdentifier)
 
 sortOnM :: (Monad m, Ord b) => (a -> m b) -> [a] -> m [a]
 sortOnM f xs = map fst . sortBy (comparing snd) . zip xs <$> mapM f xs
 
 --------------------------------------------------------------------------------
 
-sidebarCtx :: Context String -> Context String
+sidebarCtx :: Hakyll.Context String -> Hakyll.Context String
 sidebarCtx nodeCtx =
-    listField "list_pages" nodeCtx (loadAllSnapshots ("pages/*" .&&. hasNoVersion) "page-content") `mappend`
-    defaultContext
+    Hakyll.listField "list_posts" nodeCtx (Hakyll.recentFirst =<< Hakyll.loadAllSnapshots ("posts/*" Hakyll..&&. Hakyll.hasNoVersion) "content")
+    <> Hakyll.listField "list_pages" nodeCtx (Hakyll.loadAllSnapshots ("pages/*" Hakyll..&&. Hakyll.hasNoVersion) "page-content") 
+    <> Hakyll.defaultContext
 
-baseNodeCtx :: Context String
+baseNodeCtx :: Hakyll.Context String
 baseNodeCtx =
-    urlField "node-url" `mappend`
-    titleField "title" `mappend`
+    Hakyll.urlField "node-url" `mappend`
+    Hakyll.titleField "title" `mappend`
     baseCtx
 
+baseSidebarCtx :: Hakyll.Context String
 baseSidebarCtx = sidebarCtx baseNodeCtx
 
-evalCtxKey :: Context String -> [String] -> Item String -> Compiler String
-evalCtxKey context [key] item = (unContext context key [] item) >>= \cf ->
+evalCtxKey :: Hakyll.Context String -> [String] -> Hakyll.Item String -> Hakyll.Compiler String
+evalCtxKey context [key] item = Hakyll.unContext context key [] item >>= \cf ->
         case cf of
-            StringField s -> return s
+            Hakyll.StringField s -> return s
             _             -> error $ "Internal error: StringField expected"
 
-getMetadataKey :: [String] -> Item String -> Compiler String
-getMetadataKey [key] item = getMetadataField' (itemIdentifier item) key
+getMetadataKey :: [String] -> Hakyll.Item String -> Hakyll.Compiler String
+getMetadataKey [key] item = Hakyll.getMetadataField' (Hakyll.itemIdentifier item) key
